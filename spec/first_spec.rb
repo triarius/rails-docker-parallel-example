356dd73b70f5cfa97c61702c13804dff3233c8fd
Narthana Epa <narthana.epa@gmail.com>
2023-03-06 08:35:15 +1100

    Add a test that uses the database

--- spec/first_spec.rb
+++ spec/first_spec.rb
@@ -1,7 +1,14 @@
-require 'spec_helper'
+require "spec_helper"
 
-RSpec.describe "First spec" do
-  it "runs in parallel" do
-    expect(1).to eql(1)
+RSpec.describe "creates and object in the database" do
+  before :each do
+    product = Product.new
+    product.name = "New Product"
+    product.save
+  end
+
+  it "persists in the database" do
+    product = Product.find_by(name: "New Product")
+    expect(product.name).to eq("New Product")
   end
 end
